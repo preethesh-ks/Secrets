c5728d61df2c2eb8fc79e0eeb7a49324ad0cb7bd
Preethesh k.s <preetheshprezz@gmail.com>
2022-12-12 18:10:40 +0530

    level 2 auth done moongoose encryption login and register

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/userDB');  
+//   await mongoose.connect('mongodb+srv://crystal_clear:An754d57ee6gyPDg@cluster0.qzdyhxz.mongodb.net/blogDB?retryWrites=true&w=majority')
+  }
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const secret = "thisisoursecret.";
+userSchema.plugin(encrypt, { secret: secret,encryptedFields:["password"] });
+
+const User = new mongoose.model("User",userSchema);
+
+
+
 
 app.get("/",function(req,res){
     res.render("home");
@@ -18,6 +38,37 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email:req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("secrets")
+        }
+    });
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+})
 
 
 
